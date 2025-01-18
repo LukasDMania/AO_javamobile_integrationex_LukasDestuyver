718f3517faad0b26bf7d810079f8b44033986879
LukasDMania <lukasdestuyver2@gmail.com>
2025-01-18 13:23:33 +0100

    Add User and WorkOrder DAO Interfaces

--- app/src/main/java/com/examenopdracht/electroman/data/dao/UserDao.java
+++ app/src/main/java/com/examenopdracht/electroman/data/dao/UserDao.java
@@ -1,4 +1,45 @@
 package com.examenopdracht.electroman.data.dao;
 
-public class UserDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Transaction;
+import androidx.room.Update;
+
+import com.examenopdracht.electroman.data.entity.User;
+import com.examenopdracht.electroman.data.relation.UserWithWorkOrders;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Insert
+    void insertUser(User user);
+    @Update
+    void updateUser(User user);
+    @Delete
+    void deleteUser(User user);
+
+    @Query("SELECT * FROM User")
+    List<User> getAllUsers();
+    @Query("SELECT * FROM User WHERE id = :id")
+    User getUserById(Long id);
+    @Query("SELECT * FROM User WHERE userName = :userName")
+    User getUserByUserName(String userName);
+    @Transaction
+    @Query("SELECT * FROM User WHERE id = :userId")
+    UserWithWorkOrders getUserWithWorkOrders(Long userId);
+    @Transaction
+    @Query("SELECT * FROM User")
+    List<UserWithWorkOrders> getAllUsersWithWorkOrders();
+
+
+    // LiveData for obseving changes
+    @Query("SELECT * FROM User")
+    LiveData<List<User>> getAllUsersLive();
+    @Transaction
+    @Query("SELECT * FROM User")
+    LiveData<List<UserWithWorkOrders>> getAllUsersWithWorkOrdersLive();
 }
--- app/src/main/java/com/examenopdracht/electroman/data/dao/WorkOrderDao.java
+++ app/src/main/java/com/examenopdracht/electroman/data/dao/WorkOrderDao.java
@@ -1,4 +1,45 @@
 package com.examenopdracht.electroman.data.dao;
 
-public class WorkOrderDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.examenopdracht.electroman.data.entity.WorkOrder;
+
+import java.util.List;
+
+@Dao
+public interface WorkOrderDao {
+    @Insert
+    void insertWorkOrder(WorkOrder workOrder);
+    @Update
+    void updateWorkOrder(WorkOrder workOrder);
+    @Delete
+    void deleteWorkOrder(WorkOrder workOrder);
+
+    @Query("SELECT * FROM WorkOrder")
+    List<WorkOrder> getAllWorkOrders();
+    @Query("SELECT * FROM WorkOrder WHERE id = :id")
+    WorkOrder getWorkOrderById(Long id);
+    @Query("SELECT * FROM WorkOrder WHERE userId = :userId")
+    List<WorkOrder> getWorkOrdersByUserId(Long userId);
+    @Query("SELECT * FROM WorkOrder WHERE processed = :isProcessed")
+    List<WorkOrder> getWorkOrdersByProcessedStatus(boolean isProcessed);
+
+    // LiveData for observing changes
+    @Query("SELECT * FROM WorkOrder")
+    LiveData<List<WorkOrder>> getAllWorkOrdersLive();
+    @Query("SELECT * FROM WorkOrder WHERE userId = :userId")
+    LiveData<List<WorkOrder>> getWorkOrdersForUserLive(Long userId);
+
+
+    @Query("SELECT COUNT(*) FROM WorkOrder")
+    int getTotalWorkOrderCount();
+    @Query("SELECT COUNT(*) FROM WorkOrder WHERE processed = 1")
+    int getProcessedWorkOrderCount();
+    @Query("SELECT COUNT(*) FROM WorkOrder WHERE userId = :userId")
+    int getWorkOrderCountForUser(Long userId);
 }
