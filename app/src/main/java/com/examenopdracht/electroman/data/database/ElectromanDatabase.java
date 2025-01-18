c2339145a8d63711f66a8733629c2336ee03d40b
LukasDMania <lukasdestuyver2@gmail.com>
2025-01-18 13:30:27 +0100

    Add the Room database class

---
+++ app/src/main/java/com/examenopdracht/electroman/data/database/ElectromanDatabase.java
@@ -0,0 +1,35 @@
+package com.examenopdracht.electroman.data.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.examenopdracht.electroman.data.dao.UserDao;
+import com.examenopdracht.electroman.data.dao.WorkOrderDao;
+import com.examenopdracht.electroman.data.entity.User;
+import com.examenopdracht.electroman.data.entity.WorkOrder;
+
+@Database(entities = {User.class, WorkOrder.class}, version = 1)
+public abstract class ElectromanDatabase extends RoomDatabase {
+    private static volatile ElectromanDatabase INSTANCE;
+
+    public abstract UserDao userDao();
+    public abstract WorkOrderDao workOrderDao();
+
+    public static ElectromanDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (ElectromanDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            ElectromanDatabase.class,
+                            "electroman_db"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
