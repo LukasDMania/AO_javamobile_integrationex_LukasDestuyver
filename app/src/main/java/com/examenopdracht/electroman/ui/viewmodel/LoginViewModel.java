052821ce98ed077fd51888a8ca0583f5fe7569f9
LukasDMania <lukasdestuyver2@gmail.com>
2025-01-19 12:55:49 +0100

    Add login and registerfragment

--- app/src/main/java/com/examenopdracht/electroman/ui/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/examenopdracht/electroman/ui/viewmodel/LoginViewModel.java
@@ -4,19 +4,29 @@ import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.examenopdracht.electroman.data.entity.User;
 import com.examenopdracht.electroman.data.repository.UserRepository;
 
 public class LoginViewModel extends AndroidViewModel {
-    private final UserRepository userRepository;
+    private final UserRepository mUserRepository;
+    private LiveData<User> mAuthenticatedUser;
+    private MutableLiveData<String> mErrorMessage = new MutableLiveData<>();
 
     public LoginViewModel(Application application) {
         super(application);
-        userRepository = new UserRepository(application);
+        mUserRepository = new UserRepository(application);
     }
 
     public LiveData<User> authenticateUser(String userName, String password) {
         return null;
+    }
+
+    public LiveData<User> getAuthenticatedUser() {
+        return mAuthenticatedUser;
+    }
+    public LiveData<String> getErrorMessage() {
+        return mErrorMessage;
     }
 }
